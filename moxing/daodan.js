6e520f03def1844419a0d7ae75cef78a6d0ad542
fengye <nocturne.ffg@gmail.com>
2019-02-21 23:52:11 +0800

    add file

---
+++ moxing/daodan.js
@@ -0,0 +1,68 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "daodan.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 268,
+        "faces"         : 314,
+        "normals"       : 301,
+        "uvs"           : 235,
+        "materials"     : 4
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "Default1",
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.71, 0.71, 0.71],
+	"illumination" : 2,
+	"mapDiffuse" : "HongJian.jpg",
+	"opacity" : 1.0,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 50.0
+	},
+
+	{
+	"DbgColor" : 15597568,
+	"DbgIndex" : 1,
+	"DbgName" : "Material__25",
+	"colorDiffuse" : [0.588, 0.588, 0.588],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"mapDiffuse" : "HongJian.jpg",
+	"opacity" : 1.0,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0
+	},
+
+	{
+	"DbgColor" : 60928,
+	"DbgIndex" : 2,
+	"DbgName" : "Default_2",
+	"colorDiffuse" : [0.0, 0.0039, 0.0118],
+	"colorSpecular" : [0.0, 0.0, 0.0],
+	"illumination" : 2,
+	"opacity" : 0.8,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 10.0,
+	"transparent" : true
+	},
+
+	{
+	"DbgColor" : 238,
+	"DbgIndex" : 3,
+	"DbgName" : "VMtl32",
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.5624, 0.5624, 0.5624],
+	"illumination" : 2,
+	"opacity" : 1.0,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 50.0
+	}],
+
+    "buffers": "daodan.bin"
+
+}
---
+++ moxing/diji.js
@@ -0,0 +1,31 @@
+{
+
+    "metadata" :
+    {
+        "formatVersion" : 3.1,
+        "sourceFile"    : "diji.obj",
+        "generatedBy"   : "OBJConverter",
+        "vertices"      : 2191,
+        "faces"         : 4254,
+        "normals"       : 2671,
+        "uvs"           : 2035,
+        "materials"     : 1
+    },
+
+    "materials": [	{
+	"DbgColor" : 15658734,
+	"DbgIndex" : 0,
+	"DbgName" : "_diji_FA_18C_Hornetplane",
+	"colorDiffuse" : [1.0, 1.0, 1.0],
+	"colorSpecular" : [0.7529, 0.7529, 0.7529],
+	"illumination" : 2,
+	"mapBump" : "FA-18C_Hornet_N.jpg",
+	"mapDiffuse" : "FA-18C_Hornet_P01.jpg",
+	"opacity" : 1.0,
+	"opticalDensity" : 1.5,
+	"specularCoef" : 8.0
+	}],
+
+    "buffers": "diji.bin"
+
+}
